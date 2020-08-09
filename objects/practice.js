dae9d24d215a55cdb771d32a0adc19a684ea4824
Megan Olsen <megan.a.olsen63@gmail.com>
2020-08-09 12:35:42 -0600

    done objects

--- objects/practice.js
+++ objects/practice.js
@@ -165,3 +165,10 @@ function makePerson(name, birthday, ssn){
 */
 
 //Code Here
+function makeCard(cardNumber, expirationDate, securityCode){
+  return {
+    cardNumber:cardNumber,
+    expirationDate: expirationDate,
+    securityCode:securityCode
+  }
+}
